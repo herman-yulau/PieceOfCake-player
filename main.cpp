6aac3c6bb1ce3c9da48cea0e12e8a3c37f382c6c
Herman Yulau <hermanyulau@gmail.com>
2020-04-10 18:00:31 +0300

    gStreamer in qml - working example

--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickWindow>
+#include <QQuickItem>
+#include <QRunnable>
+#include <QQmlContext>
+
+#include <gst/gst.h>
+
+class SetPlaying : public QObject
+{
+    Q_OBJECT
+public:
+  SetPlaying(GstElement *);
+  ~SetPlaying();
+public slots:
+  Q_INVOKABLE void run ();
+
+private:
+  GstElement * pipeline_;
+};
+
+//--------- definitions
+SetPlaying::SetPlaying (GstElement * pipeline)
+{
+  this->pipeline_ = pipeline ? static_cast<GstElement *> (gst_object_ref (pipeline)) : NULL;
+}
+
+SetPlaying::~SetPlaying ()
+{
+  if (this->pipeline_)
+    gst_object_unref (this->pipeline_);
+}
+
+void
+SetPlaying::run ()
+{
+  if (this->pipeline_)
+    gst_element_set_state (this->pipeline_, GST_STATE_PLAYING);
+}
+// without this line class declaration in main doesn't work
+#include "main.moc"
 
 int main(int argc, char *argv[])
 {
@@ -7,8 +47,31 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
+    int ret;
+
+    gst_init (&argc, &argv);
+
+    GstElement *pipeline = gst_parse_launch("videotestsrc ! glupload ! qmlglsink name=qs", NULL);
+
+//    GstElement *src = gst_element_factory_make ("videotestsrc", NULL);
+//    GstElement *glupload = gst_element_factory_make ("glupload", NULL);
+    /* the plugin must be loaded before loading the qml file to register the
+    * GstGLVideoItem qml item */
+//    GstElement *sink = gst_element_factory_make ("qmlglsink", NULL);
+
+//    g_assert (src && glupload && sink);
+
+//    gst_bin_add_many (GST_BIN (pipeline), src, glupload, sink, NULL);
+//    gst_element_link_many (src, glupload, sink, NULL);
+
+    GstElement *sink;
+    sink = gst_bin_get_by_name(GST_BIN(pipeline), "qs");
+    g_assert(sink);
+    gst_object_unref (sink);
+
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
+
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
@@ -16,5 +79,30 @@ int main(int argc, char *argv[])
     }, Qt::QueuedConnection);
     engine.load(url);
 
-    return app.exec();
+    QQuickItem *videoItem;
+    QQuickWindow *rootObject;
+
+    /* find and set the videoItem on the sink */
+    rootObject = static_cast<QQuickWindow *> (engine.rootObjects().first());
+    videoItem = rootObject->findChild<QQuickItem *> ("videoItem");
+    g_assert (videoItem);
+    g_object_set(sink, "widget", videoItem, NULL);
+
+    // set object as context property to call from qml
+    SetPlaying *startPlay = new SetPlaying(pipeline);
+    engine.rootContext()->setContextProperty("startPlay", startPlay);
+
+//    rootObject->scheduleRenderJob (new SetPlaying (pipeline),
+//        QQuickWindow::BeforeSynchronizingStage);
+
+    ret = app.exec();
+
+    delete  startPlay;
+
+    gst_element_set_state (pipeline, GST_STATE_NULL);
+    gst_object_unref (pipeline);
+
+    gst_deinit ();
+
+    return ret;
 }
--- main.qml
+++ main.qml
@@ -1,9 +1,31 @@
 import QtQuick 2.14
 import QtQuick.Window 2.14
 
+import org.freedesktop.gstreamer.GLVideoItem 1.0
+
 Window {
     visible: true
     width: 640
     height: 480
-    title: qsTr("Hello World")
+    title: qsTr("Piece of Cake")
+
+    Item {
+        anchors.fill: parent
+
+        GstGLVideoItem {
+            id: video
+            objectName: "videoItem"
+            anchors.fill: parent
+
+            MouseArea {
+                id: mousearea
+                anchors.fill: parent
+                hoverEnabled: true
+                onClicked: {
+                    console.log("hit")
+                    startPlay.run();
+                }
+            }
+        }
+    }
 }
